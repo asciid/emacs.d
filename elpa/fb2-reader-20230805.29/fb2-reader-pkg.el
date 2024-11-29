3287786454d8888bdc2aa6bc58740f61386a0322
ash <nullmaster@pm.me>
2024-11-29 20:34:55 +0300

    new files

---
+++ elpa/fb2-reader-20230805.29/fb2-reader-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "fb2-reader" "20230805.29"
+  "Read FB2 and FB2.ZIP documents."
+  '((emacs              "26.2")
+    (f                  "0.17")
+    (s                  "1.11.0")
+    (dash               "2.12.0")
+    (visual-fill-column "2.2")
+    (async              "1.9.4"))
+  :url "https://github.com/jumper047/fb2-reader"
+  :commit "85777f99483b84f02c1abd6fe0ddbbac7f1258af"
+  :revdesc "85777f99483b"
+  :keywords '("multimedia" "ebook" "fb2")
+  :authors '(("Dmitriy Pshonko" . "jumper047@gmail.com"))
+  :maintainers '(("Dmitriy Pshonko" . "jumper047@gmail.com")))
